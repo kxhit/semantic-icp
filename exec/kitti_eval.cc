f2a87fa82db2bdf204ae603d26f3abc202d51795
Steven Parkison <sparki@umich.edu>
2017-08-30 11:48:40 -0400

    added kitti evaluation

---
+++ exec/kitti_eval.cc
@@ -0,0 +1,199 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cmath>
+#include <string>
+#include <dirent.h>
+#include <algorithm>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/gicp.h>
+#include <pcl/console/parse.h>
+
+#include <semantic_point_cloud.h>
+#include <semantic_icp.h>
+#include <semantic_viewer.h>
+#include <pcl_2_semantic.h>
+#include "kitti_metrics.h"
+
+
+
+std::vector<std::string>
+get_pcd_in_dir(std::string dir_name) {
+
+    DIR           *d;
+    struct dirent *dir;
+    d = opendir(dir_name.c_str());
+
+    std::vector<std::string> pcd_fns;
+
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+
+            // Check to make sure this is a pcd file match
+            if (std::strlen(dir->d_name) >= 4 &&
+                    std::strcmp(dir->d_name + std::strlen(dir->d_name)  - 4, ".pcd") == 0) {
+                pcd_fns.push_back(dir_name + "/" + std::string(dir->d_name));
+            }
+        }
+
+        closedir(d);
+    }
+
+    return pcd_fns;
+}
+
+int
+main (int argc, char** argv)
+{
+    std::string strDirectory;
+    std::string strGTFile;
+    if ( !pcl::console::parse_argument(argc, argv, "-s", strDirectory) ) {
+        std::cout << "Need source directory (-s)\n";
+        return (-1);
+    }
+    if ( !pcl::console::parse_argument(argc, argv, "-t", strGTFile) ) {
+        std::cout << "Need ground truth file (-t)\n";
+        return (-1);
+    }
+
+    std::vector<std::string> pcd_fns = get_pcd_in_dir(strDirectory);
+    std::sort(pcd_fns.begin(),pcd_fns.end());
+
+    std::cout << "PCD FILES\n";
+    for(std::string s: pcd_fns) {
+        std::cout << s << std::endl;
+    }
+
+    KittiMetrics semanticICPMetrics(strGTFile);
+    KittiMetrics se3GICPMetrics(strGTFile);
+    KittiMetrics GICPMetrics(strGTFile);
+
+    for(size_t n = 10; n<110; n++) {
+        std::cout << "Cloud# " << n << std::endl;
+        std::string strTarget = pcd_fns[n];
+        std::string strSource = pcd_fns[n+3];
+        //Sophus::SE3d initTransform = semanticICPMetrics.getGTtransfrom(n, n+3);
+        Eigen::Matrix4d temp = Eigen::Matrix4d::Identity();
+        Sophus::SE3d initTransform(temp);
+        pcl::PointCloud<pcl::PointXYZL>::Ptr cloudA (new pcl::PointCloud<pcl::PointXYZL>);
+
+        if (pcl::io::loadPCDFile<pcl::PointXYZL> (strSource, *cloudA) == -1) //* load the file
+        {
+            PCL_ERROR ("Couldn't read source file\n");
+            return (-1);
+        }
+
+        std::shared_ptr<semanticicp::SemanticPointCloud<pcl::PointXYZ, uint32_t>>
+            semanticA (new semanticicp::SemanticPointCloud<pcl::PointXYZ, uint32_t> ());
+
+        semanticicp::pcl_2_semantic(cloudA, semanticA);
+
+        pcl::PointCloud<pcl::PointXYZL>::Ptr cloudB (new pcl::PointCloud<pcl::PointXYZL>);
+
+        if (pcl::io::loadPCDFile<pcl::PointXYZL> (strTarget, *cloudB) == -1) //* load the file
+        {
+            PCL_ERROR ("Couldn't read target file\n");
+            return (-1);
+        }
+
+        std::shared_ptr<semanticicp::SemanticPointCloud<pcl::PointXYZ, uint32_t>>
+            semanticB (new semanticicp::SemanticPointCloud<pcl::PointXYZ, uint32_t> ());
+
+        semanticicp::pcl_2_semantic(cloudB, semanticB);
+
+        semanticicp::SemanticIterativeClosestPoint<pcl::PointXYZ, uint32_t> sicp;
+        sicp.setInputSource(semanticA);
+        sicp.setInputTarget(semanticB);
+
+        auto begin = std::chrono::steady_clock::now();
+        sicp.align(semanticA, initTransform);
+        auto end = std::chrono::steady_clock::now();
+        std::cout << "Time Multiclass: "
+                << std::chrono::duration_cast<std::chrono::seconds>(end-begin).count() << std::endl;
+        Sophus::SE3d sicpTranform = sicp.getFinalTransFormation();
+        std::cout << "SICP MSE: "
+                  << semanticICPMetrics.evaluate(sicpTranform, n, n+3)
+                  << std::endl;
+
+        for(size_t t = 0; t< cloudA->points.size(); t++){
+            pcl::PointXYZL p = cloudA->points[t];
+            p.label=0;
+            cloudA->points[t] = p;
+        }
+        std::shared_ptr<semanticicp::SemanticPointCloud<pcl::PointXYZ, uint32_t>>
+            semanticAnoL (new semanticicp::SemanticPointCloud<pcl::PointXYZ, uint32_t> ());
+
+        semanticicp::pcl_2_semantic(cloudA, semanticAnoL);
+
+
+        for(size_t t = 0; t< cloudB->points.size(); t++){
+            pcl::PointXYZL p = cloudB->points[t];
+            p.label=0;
+            cloudB->points[t] = p;
+        }
+        std::shared_ptr<semanticicp::SemanticPointCloud<pcl::PointXYZ, uint32_t>>
+            semanticBnoL (new semanticicp::SemanticPointCloud<pcl::PointXYZ, uint32_t> ());
+
+        semanticicp::pcl_2_semantic(cloudB, semanticBnoL);
+
+        semanticicp::SemanticIterativeClosestPoint<pcl::PointXYZ, uint32_t> sicp2;
+        sicp2.setInputSource(semanticAnoL);
+        sicp2.setInputTarget(semanticBnoL);
+
+        begin = std::chrono::steady_clock::now();
+        sicp2.align(semanticAnoL, initTransform);
+        end = std::chrono::steady_clock::now();
+        std::cout << "Time Single Class: "
+                  << std::chrono::duration_cast<std::chrono::seconds>(end-begin).count() << std::endl;
+        Sophus::SE3d gicpTransform = sicp2.getFinalTransFormation();
+        std::cout << "se3GICP MSE: "
+                  << se3GICPMetrics.evaluate(gicpTransform, n, n+3)
+                  << std::endl;
+
+        pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZL, pcl::PointXYZL> gicp;
+        gicp.setInputCloud(cloudA);
+        gicp.setInputTarget(cloudB);
+        pcl::PointCloud<pcl::PointXYZL> final1;
+
+        begin = std::chrono::steady_clock::now();
+        gicp.align(final1, (initTransform.matrix()).cast<float>());
+        end = std::chrono::steady_clock::now();
+        std::cout << "Time GICP: "
+                  << std::chrono::duration_cast<std::chrono::seconds>(end-begin).count() << std::endl;
+        Eigen::Matrix4f mat = gicp.getFinalTransformation();
+        std::cout << "Final GICP Transform\n";
+        std::cout << mat << std::endl;
+        Sophus::SE3d gicpTransform2(mat.cast<double>());
+        std::cout << "GICP MSE: "
+                  << GICPMetrics.evaluate(gicpTransform2, n, n+3)
+                  << std::endl;
+
+    }
+    std::cout << " SICP FINAL MSE: " << semanticICPMetrics.getTransformMSE() << std::endl;
+    std::cout << "Transform\n";
+    semanticICPMetrics.printTransfrom();
+    std::cout << "Rot\n";
+    semanticICPMetrics.printRot();
+    std::cout << "Trans\n";
+    semanticICPMetrics.printTrans();
+
+    std::cout << " se3GICP FINAL MSE: " << se3GICPMetrics.getTransformMSE() << std::endl;
+    std::cout << "Transform\n";
+    se3GICPMetrics.printTransfrom();
+    std::cout << "Rot\n";
+    se3GICPMetrics.printRot();
+    std::cout << "Trans\n";
+    se3GICPMetrics.printTrans();
+
+    std::cout << " GICP FINAL MSE: " << GICPMetrics.getTransformMSE() << std::endl;
+    std::cout << "Transform\n";
+    GICPMetrics.printTransfrom();
+    std::cout << "Rot\n";
+    GICPMetrics.printRot();
+    std::cout << "Trans\n";
+    GICPMetrics.printTrans();
+
+    return (0);
+}
---
+++ exec/kitti_metrics.h
@@ -0,0 +1,113 @@
+#ifndef _KITTI_METRICS_H_
+#define _KITTI_METRICS_H_
+
+#include "csv.h"
+
+class KittiMetrics
+{
+    public:
+
+        KittiMetrics(std::string& gtFileName) :
+            gtFile_(gtFileName),
+            count_(0),
+            transformMSE_(0),
+            rotMSE_(0),
+            transMSE_(0)
+        {
+            for(CSVIterator loop(gtFile_); loop != CSVIterator(); ++loop) {
+                Eigen::Matrix4d mat = Eigen::Matrix4d::Identity();
+                double data[12];
+                for(size_t n =0; n < (*loop).size() && n < 12; n++) {
+                    data[n] = std::stod((*loop)[n]);
+                }
+                mat.block<3,4> (0,0) = Eigen::Map<Eigen::Matrix<double,3,4,Eigen::RowMajor> > (data);
+                Sophus::SE3d trans(mat);
+                gtPoses_.push_back(trans);
+            }
+
+        };
+
+        double
+        evaluate(const Sophus::SE3d& transform, size_t poseIDA, size_t poseIDB) {
+            Sophus::SE3d transformGT = getGTtransfrom(poseIDA, poseIDB);
+            Sophus::SE3d transformDiff = transformGT*transform.inverse();
+            double transformError = transformDiff.log().squaredNorm();
+            double rotError = transformDiff.rotationMatrix().squaredNorm();
+            double transError = transformDiff.translation().squaredNorm();
+
+            transformDiffs_.push_back(transformDiff);
+            transformErrors_.push_back(transformError);
+            rotErrors_.push_back(rotError);
+            transErrors_.push_back(transError);
+
+            transformMSE_ += transformError;
+            rotMSE_ += rotError;
+            transMSE_ += transError;
+            count_++;
+
+            //std::cout << "Pose A\n";
+            //std::cout << poseA.matrix();
+            //std::cout << std::endl;
+            //std::cout << "Pose B\n";
+            //std::cout << poseB.matrix();
+            //std::cout << std::endl;
+            std::cout << "GT Transform\n";
+            std::cout << transformGT.matrix();
+            std::cout << std::endl;
+            return transformError;
+        };
+
+        Sophus::SE3d getGTtransfrom(size_t poseIDA, size_t poseIDB) {
+            const Sophus::SE3d& poseA = gtPoses_[poseIDA];
+            const Sophus::SE3d& poseB = gtPoses_[poseIDB];
+            return poseB*poseA.inverse();
+        };
+
+        double getTransformMSE() {
+            return transformMSE_/double(count_);
+        };
+
+        double getRotMSE() {
+            return rotMSE_/double(count_);
+        };
+
+        double getTransMSE() {
+            return transMSE_/double(count_);
+        };
+
+        void printTransfrom() {
+            for(double v: transformErrors_) {
+                std::cout << v << std::endl;
+            }
+        };
+
+        void printRot() {
+            for(double v: rotErrors_) {
+                std::cout << v << std::endl;
+            }
+        };
+
+        void printTrans() {
+            for(double v: transErrors_) {
+                std::cout << v << std::endl;
+            }
+        };
+
+
+    private:
+        std::ifstream gtFile_;
+        std::vector<Sophus::SE3d> gtPoses_;
+
+        std::vector<Sophus::SE3d> transformDiffs_;
+        std::vector<double> transformErrors_;
+        std::vector<double> rotErrors_;
+        std::vector<double> transErrors_;
+        double transformMSE_;
+        double rotMSE_;
+        double transMSE_;
+        size_t count_;
+};
+
+
+
+#endif // _KITTI_METRICS_H_
